81cba02c1fc0fd14f22ec63212326764d40f210c
Hayk-PG <pautikgames@gmail.com>
2023-07-06 17:06:46 -0400

    Implemented deduction of team money upon storage space usage

--- Assets/Scripts/Player/PlayerStorageFeeManager.cs
+++ Assets/Scripts/Player/PlayerStorageFeeManager.cs
@@ -1,6 +1,7 @@
+using Photon.Pun;
 using UnityEngine;
 
-public class PlayerStorageFeeManager : MonoBehaviour
+public class PlayerStorageFeeManager : MonoBehaviourPun
 {
     [Header("Components")]
     [SerializeField] private EntityManager _entityManager;
@@ -8,7 +9,8 @@ public class PlayerStorageFeeManager : MonoBehaviour
 
     private int _totalFee;
     private object[] _data = new object[1];
-
+    private object[] _storageSpaceUsageData = new object[2];
+         
     private System.Action<int> CalculateTotalFeeDelegate => CalculateTotalFee;
 
 
@@ -39,20 +41,45 @@ public class PlayerStorageFeeManager : MonoBehaviour
         }
 
         bool isMyTeamTurn = IsCurrentTurnTargetTurn((TeamIndex)data[2], _entityIndexManager.TeamIndex);
+        SubtractMoneyForStorageSpaceUsage(isMyTeamTurn);
+        RaiseFeeCalculationEvent();        
+    }
 
-        RaiseFeeCalculationEvent();
+    private bool IsCurrentTurnTargetTurn(TeamIndex currentTurn, TeamIndex targetTurn)
+    {
+        return currentTurn == targetTurn;
+    }
+
+    private void SubtractMoneyForStorageSpaceUsage(bool isMyTeamTurn)
+    {
+        bool canProcess = !isMyTeamTurn && _totalFee > 0;
+
+        if (!canProcess)
+        {
+            return;
+        }
+
+        photonView.RPC("SubtractMoneyForStorageSpaceUsageRPC", RpcTarget.AllViaServer, (short)-_totalFee, (byte)_entityIndexManager.TeamIndex);
         ResetTotalFee();
+    }
+
+    [PunRPC]
+    private void SubtractMoneyForStorageSpaceUsageRPC(short moneyAmount, byte teamIndex)
+    {
+        _storageSpaceUsageData[0] = moneyAmount;
+        _storageSpaceUsageData[1] = (TeamIndex)teamIndex;
+        GameEventHandler.RaiseEvent(GameEventType.UpdateMoneyRegardlessOfSale, _storageSpaceUsageData);
+
+        if((TeamIndex)teamIndex == _entityIndexManager.TeamIndex)
+        {
+            PlaySoundEffect(5, 0);
+        }
     }
 
     private void RaiseFeeCalculationEvent()
     {
         _data[0] = CalculateTotalFeeDelegate;
         GameEventHandler.RaiseEvent(GameEventType.CalculateStorageSpaceFee, _data);
-    }
-
-    private bool IsCurrentTurnTargetTurn(TeamIndex currentTurn, TeamIndex targetTurn)
-    {
-        return currentTurn == targetTurn;
     }
 
     private void CalculateTotalFee(int storageSpaceFeeAmount)
@@ -64,5 +91,11 @@ public class PlayerStorageFeeManager : MonoBehaviour
     private void ResetTotalFee()
     {
         _totalFee = 0;
+    }
+
+    private void PlaySoundEffect(int listIndex, int clipIndex)
+    {
+        UISoundController.PlaySound(listIndex, clipIndex);
+
     }
 }
