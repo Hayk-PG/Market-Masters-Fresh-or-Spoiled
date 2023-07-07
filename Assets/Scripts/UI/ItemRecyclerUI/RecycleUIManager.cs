f3cd0b1cdb681d26785b48bbedf21d522ade27f3
Hayk-PG <pautikgames@gmail.com>
2023-07-07 06:05:48 -0400

    Improved sound effect handling and UI updating in the 'RecycleUIManager' script

--- Assets/Scripts/UI/ItemRecyclerUI/RecycleUIManager.cs
+++ Assets/Scripts/UI/ItemRecyclerUI/RecycleUIManager.cs
@@ -1,12 +1,30 @@
 using UnityEngine;
+using UnityEngine.UI;
+using Pautik;
 
 public class RecycleUIManager : InventoryItemDragDropUIResponder
 {
+    [Header("Sprites For Icon")]
+    [SerializeField] private Sprite _iconClosedRecycleBinSprite;
+    [SerializeField] private Sprite _iconOpenedRecycleBinSprite;
+
+    [Header("UI Elements")]
+    [SerializeField] private Btn_Icon _icon;
+
+    private Image _iconImage;
     private int _recyclingStartTurnCount = 0;
     private int _availableRecyclingTurnCount = 0;
+    
+    private bool HasItem => _inventoryItemButton != null;
+    private bool CanRecycle => GameSceneReferences.Manager.GameTurnManager.TurnCount >= _availableRecyclingTurnCount || GameSceneReferences.Manager.GameTurnManager.TurnCount <= _recyclingStartTurnCount;
 
 
 
+
+    private void Awake()
+    {
+        _iconImage = Get<Image>.From(_icon.gameObject);
+    }
 
     protected override void ExecuteOnDragRelease(object[] data)
     {
@@ -16,20 +34,34 @@ public class RecycleUIManager : InventoryItemDragDropUIResponder
 
     protected override void SetButtonSprites(Sprite sprite)
     {
-        
+        base.SetButtonSprites(sprite);
+
+        if (!CanRecycle && _iconImage.sprite == _iconClosedRecycleBinSprite)
+        {
+            return;
+        }
+
+        SetIconSprites(sprite);
+    }
+
+    private void SetIconSprites(Sprite buttonSprite)
+    {
+        _icon.IconSpriteChangeDelegate(buttonSprite == _defaultSprite ? _iconClosedRecycleBinSprite : _iconOpenedRecycleBinSprite);
+        _icon.ChangeReleasedSpriteDelegate();
     }
 
     private void RecycleItem()
     {
-        bool hasItem = _inventoryItemButton != null;
-        bool canRecycle = GameSceneReferences.Manager.GameTurnManager.TurnCount >= _availableRecyclingTurnCount ||
-                          GameSceneReferences.Manager.GameTurnManager.TurnCount <= _recyclingStartTurnCount;
-
-        if (hasItem && canRecycle)
+        if (HasItem && CanRecycle)
         {
             DefineRecyclingStartTurnCount();
             DefineAvailableRecyclingTurnCount();
             DestroyItemFromInventory();
+            PlaySoundEffect(1, 4);
+        }
+        else
+        {
+            PlaySoundEffect(4, 1);
         }
     }
 
@@ -46,5 +78,10 @@ public class RecycleUIManager : InventoryItemDragDropUIResponder
     private void DestroyItemFromInventory()
     {
         _inventoryItemButton.DestroySpoiledItemOnSeparateSale();
+    }
+
+    private void PlaySoundEffect(int listIndex, int clipIndex)
+    {
+        UISoundController.PlaySound(listIndex, clipIndex);
     }
 }
